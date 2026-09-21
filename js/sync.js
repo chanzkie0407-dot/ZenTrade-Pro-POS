f9e978c4e44407e10ecbcb3ca51eed067de9e7e8
chanzkie0407-dot <chanzkie0407@gmail.com>
2026-09-21 20:51:59 +0800

    Create sync.js

---
+++ js/sync.js
@@ -0,0 +1,9 @@
+  config: {
+    apiKey: "AIzaSyAxuB4gacmWIf3U4Ic5TsOxSHCaynR0vhw",
+    authDomain: "bpos-pos.firebaseapp.com",
+    projectId: "bpos-pos",
+    storageBucket: "bpos-pos.firebasestorage.app",
+    messagingSenderId: "1024558399693",
+    appId: "1:1024558399693:web:13412d95960d8be732a984",
+    measurementId: "G-BY03FZEN6E"
+  },
